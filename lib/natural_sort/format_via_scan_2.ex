ff0c7e9fea0813efca81a64b1ea57574297980bf
DanCouper <danielcouper@sutor-design.com>
2015-05-16 10:40:07 +0100

    Current limit to micro optimisation

--- lib/natural_sort/format_via_scan_2.ex
+++ lib/natural_sort/format_via_scan_2.ex
@@ -1,14 +1,11 @@
 defmodule NaturalSort.FormatViaScan2 do
-  # Order of operations:
-  # 1. Downcase if flag set
-  # 2. Break string into groups of num/letters in a list
-  # 3. Convert number strings to integers
 
   def format_item(item, case_sensitive?) when is_number(item), do: item
   def format_item(item, case_sensitive?) do
     item
     |> format_case(case_sensitive?)
-    |> string_scan(~r/([0-9]+)|([a-zA-Z]+)/)
+    # NOTE uses [relatively slow] unicode flag in regex.
+    |> string_scan(~r/(\p{Nd}+)|(\p{L}+)/u)
     |> List.flatten
     |> Enum.map(fn item -> convert_integers(item) end)
   end
@@ -21,11 +18,12 @@ defmodule NaturalSort.FormatViaScan2 do
   end
 
   defp convert_integers(string) do
-    case Regex.match?(~r/[0-9]+/, string) do
+    # NOTE uses [relatively slow] unicode flag in regex.
+    case Regex.match?(~r/\p{Nd}+/u, string) do
       true  -> String.to_integer(string)
       false -> string
     end
   end
 
-  defp string_scan(string, regex), do: Regex.scan(regex, string)
+  defp string_scan(string, regex), do: Regex.scan(regex, string, capture: :all_but_first)
 end
